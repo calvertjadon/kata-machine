8d04ae011f90f8da50a5914e1125e062e6f193c3
Jadon Calvert <calvertjadon@gmail.com>
2023-11-27 15:44:04 -0500

    add a few more algos

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -1,27 +1,78 @@
 export default class ArrayList<T> {
     public length: number;
+    private capacity: number;
+    private arr: Array<T>;
 
-    
 
-    constructor() {
+    constructor(size: number) {
+        this.length = 0;
+        this.capacity = size;
+        this.arr = Array<T>(this.capacity);
+    }
+
+    private grow(): void {
+        this.capacity *= 2;
+
+        const new_arr = Array<T>(this.capacity);
+        for (let i = 0; i < this.length; i++) {
+            new_arr[i] = this.arr[i];
+        }
+        this.arr = new_arr;
+    }
+
+    private indexOf(item: T): number {
+        for (let i = 0; i < this.length; i++) {
+            if (this.arr[i] == item) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    insertAt(item: T, idx: number): void {
+        if (this.length === this.capacity) {
+            this.grow();
+        }
+
+        for (let i = this.length; i > idx; --i) {
+            this.arr[i] = this.arr[i - 1];
+        }
+        this.arr[idx] = item;
+        this.length++;
     }
 
     prepend(item: T): void {
+        this.insertAt(item, 0);
+    }
 
-}
-    insertAt(item: T, idx: number): void {
-
-}
     append(item: T): void {
+        this.insertAt(item, this.length);
+    }
 
-}
-    remove(item: T): T | undefined {
-
-}
     get(idx: number): T | undefined {
+        if (idx < this.length) {
+            return this.arr[idx];
+        }
 
-}
+        return undefined;
+    }
+
     removeAt(idx: number): T | undefined {
+        if (idx === -1) {
+            return undefined;
+        }
 
-}
+        const item = this.arr[idx];
+        for (let j = idx; j < this.length - 1; j++) {
+            this.arr[j] = this.arr[j + 1];
+        }
+
+        this.length--;
+        return item;
+    }
+
+    remove(item: T): T | undefined {
+        let idx = this.indexOf(item);
+        return this.removeAt(idx);
+    }
 }
--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,28 @@
+function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][]): boolean {
+    // 1. base case
+    // off the map
+    if (curr.x < 0 || curr.x >= maze[0].length ||
+        curr.y < 0 || curr.y >= maze.length) {
+
+        return false;
+    }
+
+    // on a wall
+    if (maze[curr.y][curr.x] === wall) {
+        return false;
+    }
+
+    // is the end
+    if (curr.x === end.x && curr.y === end.y) {
+        return true;
+    }
+
+    // have seen current point before
+    if (seen[curr.y][curr.x]) {
+        return false;
+    }
+}
+
 export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
 
 }
