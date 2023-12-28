3cba8f285c0e9821bfaa4f5a0a63225e73ca0bfb
Jadon Calvert <calvertjadon@gmail.com>
2023-12-28 17:40:58 -0500

    arbor day

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,21 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    if (!curr) {
+        return path;
+    }
 
+    // recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+
+    // pre
+    path.push(curr.value); // visit node
+
+    // recurse
+
+    // post
+    return path
+}
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
